1653af238f4dece88d1ed4eef45e4d81a82c220a
W0738386 <W0738386@ADL-A0214Z-20>
2023-10-10 10:11:28 -0400

    Add project files.

---
+++ Luksus/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Luksus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
